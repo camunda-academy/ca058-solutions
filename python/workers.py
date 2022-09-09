5041a72f726f8c8eea1e4dc8acea8efc546f156e
shaarmann <stephan.haarmann@camunda.com>
2022-09-09 08:38:45 +0200

    Added logic to workers

--- python/workers.py
+++ python/workers.py
@@ -1,6 +1,12 @@
 import asyncio
 from pyzeebe import ZeebeWorker, create_camunda_cloud_channel
 
+def get_customer_credit(customerId):
+    return int(customerId[-2:]) # get the last two characters of the customer ID and convert them to int
+
+def deduct_credit(customerCredit, amountToDeduct):
+    return max(0.0, amountToDeduct - customerCredit) # calculate the openAmount
+
 # main function called by the program
 async def main():
     # create a channel to Camunda 8 SaaS
@@ -16,8 +22,10 @@ async def main():
 
     # Create a worker for task type "credit-deduction"
     @worker.task(task_type="credit-deduction")
-    def deduct_amount(customerCredit, orderTotal) -> dict: # parameters are matched to process variables, i.e., customerCredit holds the value of the process variable 'customerCredit'. If no such variable exists, an exception occurs.
-        return {"customerCredit": max(0.0, customerCredit - orderTotal), "openAmount": max(orderTotal - customerCredit, 0,0)}
+    def deduct_amount(customerId, orderTotal) -> dict: # parameters are matched to process variables, i.e., customerCredit holds the value of the process variable 'customerCredit'. If no such variable exists, an exception occurs.
+        customerCredit = get_customer_credit(customerId)
+        openAmount = deduct_credit(customerCredit, orderTotal)
+        return {"customerCredit": max(0.0, customerCredit - orderTotal), "openAmount": openAmount}
     
     # Create a worker for task type "credit-card-charging"
     @worker.task(task_type="credit-card-charging")
